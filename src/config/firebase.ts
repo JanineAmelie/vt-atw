c4e2d0dadc8d8b2363f72263fb5502e40bcb79ac
neen <tamarind11@yahoo.com>
2022-09-26 13:03:10 +0200

    chore: reinstall app again

---
+++ src/config/firebase.ts
@@ -0,0 +1,20 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+import '@firebase/auth';
+import 'firebase/auth';
+import config from './config';
+
+const Firebase = firebase.initializeApp(config.firebase);
+
+// Add or Remove authentification methods here.
+export const Providers = {
+    google: new firebase.auth.TwitterAuthProvider(),
+    // facebook: new firebase.auth.FacebookAuthProvider(),
+    // google: new firebase.auth.GoogleAuthProvider(),
+    // facebook: new firebase.auth.FacebookAuthProvider(),
+};
+
+export const auth = firebase.auth();
+export default Firebase;
