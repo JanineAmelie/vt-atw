ded96fd6726031b66a3eaea9313a66e0d3f926ee
neen <tamarind11@yahoo.com>
2022-09-29 20:31:15 +0200

    feat: remove globe spinning :(

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -18,22 +18,13 @@ interface IMapProps {
 }
 
 const INITIAL_VIEW_STATE = {
-  longitude: -122.4,
-  latitude: 37.8,
+  longitude: 0,
+  latitude: 0,
   zoom: 3
 };
 
 const MapView: React.FunctionComponent<IMapProps> = ({ id, mapStyleURL, mapboxToken }) => {
   const mapRef = useRef<MapRef>(null);
-  const [userInteracting, setUserInteracting] = useState<boolean>(false);
-  const [spinEnabled, setSpinEnabled] = useState<boolean>(true);
-
-  // At low zooms, complete a revolution every two minutes.
-  const secondsPerRevolution = 120;
-  // Above zoom level 5, do not rotate.
-  const maxSpinZoom = 5;
-  // Rotate at intermediate speeds between zoom levels 3 and 5.
-  const slowSpinZoom = 3;
 
   /** MAP EVENTS */
   const onClick = (event: any): void => {
@@ -57,86 +48,18 @@ const MapView: React.FunctionComponent<IMapProps> = ({ id, mapStyleURL, mapboxTo
     }
   };
 
-  const spinGlobe = (): void => {
-    if (mapRef.current) {
-      console.log("userInteracting", userInteracting);
-      const zoom = mapRef.current.getZoom();
-      if (spinEnabled && !userInteracting && zoom < maxSpinZoom) {
-        let distancePerSecond = 360 / secondsPerRevolution;
-        if (zoom > slowSpinZoom) {
-          // Slow spinning at higher zooms
-          const zoomDif = (maxSpinZoom - zoom) / (maxSpinZoom - slowSpinZoom);
-          distancePerSecond *= zoomDif;
-        }
-        const center = mapRef.current.getCenter();
-        const targetLocation: LngLatLike = {
-          lat: center.lat,
-          lng: center.lng - distancePerSecond
-        };
-        // center.lng -= distancePerSecond;
-        // Smoothly animate the map over one second.
-        // When this animation is complete, it calls a 'moveend' event.
-        mapRef.current?.easeTo({ center: targetLocation, duration: 1000, easing: (n) => n });
-      }
-    }
-  };
-
-  const onMapLoad = useCallback(() => {
-    const mapObject = mapRef.current;
-    if (mapObject) {
-      // Pause spinning on interaction
-      mapObject.on("mousedown", () => {
-        console.log(this);
-        console.log("mousing");
-        // setUserInteracting(true);
-        setUserInteracting((userInteracting) => true);
-        console.log(userInteracting);
-      });
-
-      // Restart spinning the globe when interaction is complete
-      mapObject.on("mouseup", () => {
-        setUserInteracting(false);
-        spinGlobe();
-      });
-
-      // These events account for cases where the mouse has moved
-      // off the map, so 'mouseup' will not be fired.
-      mapObject.on("dragend", () => {
-        setUserInteracting(false);
-        spinGlobe();
-      });
-      mapObject.on("pitchend", () => {
-        setUserInteracting(false);
-        spinGlobe();
-      });
-      mapObject.on("rotateend", () => {
-        setUserInteracting(false);
-        spinGlobe();
-      });
-
-      // When animation is complete, start spinning if there is no ongoing interaction
-
-      mapObject.on("moveend", () => {
-        console.log("moveend");
-        spinGlobe();
-      });
-    }
-    spinGlobe();
-  }, []);
-
   const [viewState, setViewState] = useState(INITIAL_VIEW_STATE);
   return (
     <MapProvider>
       <Map
         id={id}
-        initialViewState={INITIAL_VIEW_STATE}
+        initialViewState={viewState}
         onMove={(event: any) => setViewState(event.viewState)}
         style={{ width: "100vw", height: "100vh" }}
         mapStyle={mapStyleURL}
         mapboxAccessToken={mapboxToken}
         interactiveLayerIds={[clusterLayer.id || ""]}
         onClick={onClick}
-        onLoad={onMapLoad}
         projection="globe"
         ref={mapRef}
         onRender={(event) => event.target.resize()}>
