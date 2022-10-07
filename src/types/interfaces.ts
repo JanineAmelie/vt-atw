9aee0ecc43c3060136897fd94cede58ba4a26fc2
neen <tamarind11@yahoo.com>
2022-10-07 20:36:01 +0200

    chore: organized types

---
+++ src/types/interfaces.ts
@@ -0,0 +1,22 @@
+import { PinType } from "./types";
+
+export interface IMapProps {
+  id: string;
+  mapStyleURL: string;
+  mapboxToken: string;
+}
+export interface IApplicationProps {
+  name?: string;
+}
+
+export interface IPopupProps {
+  longitude: number;
+  latitude: number;
+  twitterHandle: string;
+  image: string;
+  onCloseCallback: () => void;
+}
+
+export interface IMapPinProps extends PinType {
+  onClick: () => void;
+}
---
+++ src/types/types.ts
@@ -0,0 +1,7 @@
+export type PinType = {
+  id: string;
+  longitude: number;
+  latitude: number;
+  image: string;
+  twitterHandle: string;
+};
